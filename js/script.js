d7848187efaca47da206e0dbaf54581a216b9498
ibba1 <vishal32saini@gmail.com>
2024-03-11 20:35:22 +0530

    The Diet CasCade

--- js/script.js
+++ js/script.js
@@ -331,22 +331,28 @@ function toggleChatbox() {
     chatbox.style.display = isChatboxVisible ? "block" : "none";
 }
 
-function openWhatsApp() {
-    var phoneNumber = '+91 9004491160';
-    var message = encodeURIComponent("Heyy!! I'm interested in diet counseling and seeking information on available services");
 
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+function openWhatsApp(phoneNumber, message) {
+  var isMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-    var whatsappLink = isMobile ?
-        'https://wa.me/' + phoneNumber + '?text=' + message :
-        'https://web.whatsapp.com/send?phone=' + phoneNumber + '&text=' + message;
+  var whatsappLink = isMobile ?
+      'https://wa.me/' + phoneNumber + '?text=' + message :
+      'https://web.whatsapp.com/send?phone=' + phoneNumber + '&text=' + message;
 
-    window.open(whatsappLink, '_blank');
+  if (isMobile && /(iPad|iPhone|iPod)/g.test(navigator.userAgent)) {
+      // Use Universal Links for iOS
+      window.location.href = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + message;
+  } else {
+      // Open in a new window for other devices
+      window.open(whatsappLink, '_blank');
+  }
 
-    setTimeout(function() {
-        window.location.href = '';
-    }, 2000);
-}  
+  // Keep the setTimeout for redirecting
+  setTimeout(function () {
+      window.location.href = '';
+  }, 2000);
+}
+
 
 function openWpToBookAppo() {
     var phoneNumber = '+91 9004491160';
